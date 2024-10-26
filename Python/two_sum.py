47b252ad1b26159e32c5c0b6f7a972286b09a7f2
Iftakhar Shams <55760905+iftakharshams@users.noreply.github.com>
2024-10-26 20:34:28 +1030

    python solution

--- Python/two_sum.py
+++ Python/two_sum.py
@@ -1,10 +1,13 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
         seen = {}
-        for i in range (len(nums)):
+        for i in range(len(nums)):
             diff = target - nums[i]
             if diff in seen:
                 return [seen[diff], i]
             else:
                 seen[nums[i]] = i
-                
+
+solution = Solution()
+result = solution.twoSum([2, 7, 11, 15], 9)
+print(result)
